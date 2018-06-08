ffaac4a634ca2e18591103f6759b6166cfd85c47
viviann224 <viviann224@gmail.com>
2018-06-08 15:54:53 -0500

    updated modals. need to fix js for click here

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -23,9 +23,11 @@ $(document).ready(function()
   return false;
   });
   //login / logout modal
-  $(document).ready(function(){
+
    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-   $('.modal-trigger').leanModal();
- });
+  $('.modal-trigger').leanModal();
+   // $('.loginModal').leanModal();
+   //  $('.signupModal').leanModal();
+
 
 });
